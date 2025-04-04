b912819b75dfb72a53be66f90d517edc1870cd9b
Houillier Thomas <thomas.houillier@student-cs.fr>
2025-04-04 13:50:16 +0200

    initial commit

---
+++ Essec J1/Scripts/traite_data_graid.py
@@ -0,0 +1,226 @@
+import csv
+import os 
+import pandas as pd
+import Scripts.utils as u
+
+def create_total_data():
+    L_data_graid = []
+    for file_name in os.listdir("""Resultat_brut/Data_graids"""):
+        print(file_name)
+        L_data_graid.append(pd.read_csv("""Resultat_brut/Data_graids/""" + file_name, sep = ";"))
+    res = pd.concat(L_data_graid)
+    (res.drop(res.columns[0], axis=1)).to_csv("Resultat_brut/data_graid_final.csv", sep = ";") 
+
+def traite_data_graid(parcours):
+
+    """
+    Prend en entrée le fichier datas_graid.csv et retourne un dictionnaire avec comme clés le numéro de doigts de l'équipe et comme valeur une liste de couple badgeuse/temps 
+    """
+    D_correction = traite_data_correction('Resultat_brut/')
+
+    with open('Resultat_brut/datas_graid.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter = '@')
+        data = list(reader)
+        data_par_equipe = {}
+        for i in range(1,len(data)):
+            row = data[i]
+            temps = row[0].split(';')
+            if temps[2] not in data_par_equipe:
+                equipe = int(temps[2])
+                data_par_equipe[equipe] = []
+                res_a_clear = []
+                for i in range(len(temps) - 44): #int(temps[44])
+                    if 47+3*i <len(temps) and temps[45+3*i] != "":
+                        num_badgeuse = int(temps[45+3*i])
+                        temps_badgeuse = u.sec_from_heure(temps[47+3*i])
+                        res_a_clear.append((num_badgeuse, temps_badgeuse))
+                    
+                res_clear = clear_anomalies(res_a_clear)
+                res_completement_clear = insert_correction(equipe, res_clear, D_correction)
+                L_badgeuse = parcours.get_badgeuse(res_completement_clear)
+                # print([L_badgeuse[i][0].signaleur for i in range(len(L_badgeuse))])
+                # a=1/0
+                for badgeuse, temps_badgeuse in L_badgeuse:
+                    data_par_equipe[equipe].append([badgeuse, temps_badgeuse])
+                    
+            else:
+                print("on rajoutera une erreur après")
+                a=1/0
+        return data_par_equipe
+
+
+def clear_anomalies(res_equipe):
+    """
+    fonction qui vient clear les différents résultats en supprimant les occurences trop proches de deux bipages de badgeuse. On dit que 2 itérations sont trop proches si elles sont séparés de moins de 10s. On supprime alors la deuxième itération
+    """
+    res_equipe_clear = []
+    i = 0
+    while i <len(res_equipe)-1:
+        (num_badgeuse,temps) = res_equipe[i]
+        res_equipe_clear.append((num_badgeuse,temps))
+        (num_badgeuse_suivante, temps_suivant) = res_equipe[i+1]
+        if num_badgeuse_suivante == num_badgeuse:
+            if abs(temps-temps_suivant)<=10:
+                i+=1
+        i+=1
+    if i == len(res_equipe)-1:
+        (num_badgeuse,temps) = res_equipe[i]
+        res_equipe_clear.append((num_badgeuse,temps))
+    
+
+    return res_equipe_clear
+
+
+def gather_results(parcours):
+
+    """
+    doigts est un dictionnaire avec comme clé le numéro de doigts de l'équipe et comme valeur une liste de couple badgeuse/temps
+    On récupère les données de la badge, que l'on trie par épreuve
+    On a la structure suivante :
+    - on a un dictionnaire avec comme clé les épreuves
+    - pour chaque épreuve, on a un dictionnaire avec comme clé les badgeuses et comme valeur une liste de couple numero de badgeuse/temps
+    """
+
+    doigts = traite_data_graid(parcours)
+    res = {}
+
+    for epreuve in parcours.epreuves:
+        if epreuve.type in ["obli", "bo", "grimpeur"]:
+            res[epreuve] = {}
+            for num_equipe in doigts.keys():
+                res[epreuve][num_equipe] = []
+
+                for badgeuse, temps in doigts[num_equipe]:
+                    if badgeuse.signaleur in [epreuve.ordre_badgeuse[i].signaleur for i in range(len(epreuve.ordre_badgeuse))]:
+                        res[epreuve][num_equipe].append((badgeuse, temps))   
+
+                res[epreuve][num_equipe]= epreuve.traite_doigts(res[epreuve][num_equipe], num_equipe)
+
+        if epreuve.type == "acti":
+            res[epreuve]={}
+        
+        if epreuve.type == "co":
+            res[epreuve]={}
+            
+    return res
+
+
+def traite_data_actis(path):
+
+    # Charger le fichier Excel avec toutes les feuilles
+        file_path = path + "datas_actis.xlsx"  # Remplace par ton fichier
+        sheets = pd.read_excel(file_path, sheet_name=None)  # Lire toutes les feuilles en dictionnaire
+
+        # Dictionnaire pour stocker les résultats de chaque feuille
+        result = {}
+
+        # Traiter chaque feuille individuellement
+        for sheet_name, df in sheets.items():
+            key_column = df.columns[0]  # La première colonne est la clé
+            
+            # Construire le dictionnaire pour cette feuille
+            bool_dict = {int(row[key_column]): row.iloc[1:].notna().tolist()  for _, row in df.iterrows()}
+
+            # Ajouter au résultat global
+            result[sheet_name] = bool_dict
+
+        return {nom_acti:{num_equipe : u.maj_liste_acti(result[nom_acti][num_equipe]) for num_equipe in result[nom_acti].keys()} for nom_acti in result.keys()}
+
+def traite_datas_co(path):
+    """
+    Lit un fichier CSV et retourne un dictionnaire associant chaque numéro d'équipe à son score total.
+    
+    :param nom_fichier: Nom du fichier CSV
+    :return: Dictionnaire {numero_equipe: score_total}
+    """
+    scores = {}
+    file_path = path + "datas_co.csv"
+    
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        data = list(reader)
+        for i in range(1,len(data)):
+            row = data[i]
+            if len(row) >= 2:  # Vérifier qu'il y a bien au moins 2 colonnes
+                num_equipe = int(row[0])
+                try:
+                    score = int(row[1])  # Convertir le score en nombre flottant
+                    scores[num_equipe] = scores.get(num_equipe, 0) + score  # Ajouter au score total
+                except ValueError:
+                    print(f"Valeur invalide pour le score : {row[1]}")
+    
+    return scores
+
+
+def traite_data_correction(path):
+
+    """
+    on structure les corrections sous la forme 
+    equipe -> (numero badgeuse - occurence) -> le temps 
+    """
+
+    correction = {}
+    file_path = path + "datas_correction.csv"
+    
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        data = list(reader)
+        for i in range(1,len(data)):
+            row = data[i]
+            if len(row) == 4:  # Les colonnes prises encompte ne sont que les colonnes pleines
+                num_doigts = int(row[0])
+                if not num_doigts in correction.keys():
+                    correction[num_doigts] = {}
+
+                num_badgeuse = int(row[1])  # Convertir le score en nombre flottant
+                occurence = int(row[2])
+                heure = u.sec_from_heure(row[3])
+                correction[num_doigts][(num_badgeuse, occurence)] = heure    
+    return correction
+
+def insert_correction(num_badgeuse, L_badgeuse, D_correction): #au moment d'insérer les corrections, on doit mettre d'abord les suppressions puis les ajouts de badgeuse pour une équipe
+
+    if num_badgeuse in D_correction.keys(): #si l'équipe a en effet des corrections apportés à son data graid 
+        for (badgeuse, occurence),temps in D_correction[num_badgeuse].items():
+
+
+            peut_inser = False  #on ne s'autorise à insérer les badgeuses que si les temps ne font pas n'importe quoi
+            doit_supprimer = temps ==None
+
+            if doit_supprimer:
+                deja_vu = 0
+            else:
+                deja_vu = 1
+
+            for  i in range(len(L_badgeuse)):
+                (num_L_badgeuse,temps_badgeuse) = L_badgeuse[i]
+                if peut_inser and not doit_supprimer and temps>=temps_badgeuse and (i == len(L_badgeuse)-1 or temps <= L_badgeuse[i+1][1]):
+                    L_badgeuse.insert(i+1, (badgeuse, temps))
+                    break
+                if num_L_badgeuse == badgeuse:
+                    deja_vu +=1
+                if deja_vu == occurence:
+                    peut_inser = True
+                    deja_vu+=1
+                    if doit_supprimer:
+                        L_badgeuse.pop(i)
+                        break
+    return L_badgeuse
+
+
+def traite_datas_penalties(path):
+
+    penalties = {}
+    file_path = path + "datas_penalties.csv"
+    
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        data = list(reader)
+        for i in range(1,len(data)):
+            row = data[i]
+            num_equipe = int(row[0])
+            penalties[num_equipe] = 0
+            if len(row) != 1 and row[1] != '':
+                penalties[num_equipe] = int(row[1])
+
+    return penalties
---
+++ Essec J2/Scripts/data_parcours.py
@@ -0,0 +1,353 @@
+import pandas as pd
+import Scripts.utils as u
+
+
+class Badgeuse():
+
+    def __init__(self, numero, fonction, signaleur, gain_temps=0, obligatoire = False, repetable=False, h_mass_start=None):
+
+        """
+        numero est le numero de la badgeuse
+        fonction est la fonction de la badgeuse dans la liste ["depart", "fin", "gel", "degel", "gel_1_point", "entre_bo", "debut_grimpeur", "fin_grimpeur"]
+        gain_temps est le gain de temps éventuel associé à la badgeuse
+        signaleur est le signaleur qui a la badgeuse   
+        repetable est en entier qui indique quelle occurence de la badgeuse on doit prendre en compte      
+        """
+
+        self.numero = numero
+        self.gain_temps = gain_temps
+        self.signaleur = signaleur
+        self.fonction = fonction
+        self.repetable = repetable
+        self.h_mass_start = h_mass_start
+        self.obligatoire = obligatoire
+
+    
+    def __str__(self):
+        return f"Badgeuse {self.numero}"
+        
+        
+
+
+
+class data_epreuve():
+
+    """
+    les data des epreuves sont renseignés dans un csv avec les colonnes :
+    nom;participation;victoire;record;temps_ref;points_gain_min;points_perte_min;type
+    """
+
+    def __init__(self, nom):
+        self.nom = nom 
+        self.type =""
+        pass
+
+    def __str__(self):
+        return f"Epreuve {self.nom}"
+
+            
+
+class data_epreuve_acti(data_epreuve):
+
+    def __init__(self, nom, participation, victoire, record):
+        super().__init__(nom)
+        self.type = "acti"
+        self.victoire = victoire
+        self.record = record
+        self.participation = participation
+    
+
+
+
+class data_epreuve_co(data_epreuve):
+
+    def __init__(self, nom,participation):
+        super().__init__(nom)
+        self.type = "co"
+        self.nom = nom
+        self.participation = participation
+
+
+class data_epreuve_avec_doigts(data_epreuve):
+
+    def __init__(self, nom):
+        super().__init__(nom)
+    
+    def check(self, nom, L_badgeuse):
+        """
+        L_badgeuse est une liste de badgeuse
+        """
+        if not self.is_L_badgeuse_valide(L_badgeuse):
+            raise ValueError(f"erreur dans la construction de l'épreuve {nom}, on a {[L_badgeuse[i].numero for i in range(len(L_badgeuse))]}")
+
+            print(f"erreur dans la construction de l'épreuve {nom}, on a {[L_badgeuse[i].numero for i in range(len(L_badgeuse))]}")
+            a=1/0
+
+    def is_L_badgeuse_valide(self, L_badgeuse):
+        pass
+
+  
+    
+    def count_badgeuse(self, L_badgeuse_final, equipe):
+        """
+        on vérifie que l'on a bien le bon nombre de badgeuses pour une épreuve donnée
+        """
+        D_test = {self.ordre_badgeuse[i].signaleur:0 for i in range(len(self.ordre_badgeuse)) if self.ordre_badgeuse[i].obligatoire}
+        for (badgeuse, temps) in L_badgeuse_final: 
+            if badgeuse.obligatoire:
+                D_test[badgeuse.signaleur] +=1
+
+        D_ref = {self.ordre_badgeuse[i].signaleur:0 for i in range(len(self.ordre_badgeuse)) if self.ordre_badgeuse[i].obligatoire}
+        for bagdgeuse in self.ordre_badgeuse:
+            if badgeuse.obligatoire:
+                D_ref[bagdgeuse.signaleur] += 1
+
+        for signaleur in D_ref.keys():
+            if D_test[signaleur] !=D_ref[signaleur]:
+                raise ValueError(f"il y a un problème avec la badgeuse avec le signaleur {signaleur} pour l'épreuve {self.nom}: on trouve {D_test[signaleur]} occurences pour le doigt {equipe} ")
+
+
+class data_obli(data_epreuve_avec_doigts):
+
+    def __init__(self, nom, L_badgeuse):   
+        super().__init__(nom)
+        self.type = "obli"
+        self.check(nom, L_badgeuse)
+        self.ordre_badgeuse = L_badgeuse
+
+    
+    def is_L_badgeuse_valide(self, L_badgeuse):
+        """
+        on vérifie si on a bien un départ, une arrivée, si un gel est toujours suivi d'un dégel unique
+        """
+        if len(L_badgeuse)>=2:
+            res = L_badgeuse[0].fonction == "depart" and L_badgeuse[-1].fonction == "fin"
+            i = 1
+            while i <len(L_badgeuse) and res:
+                if L_badgeuse[i].fonction == "gel":
+                    res = L_badgeuse[i+1].fonction == "degel"
+                    i+=2
+                else:
+                    i+=1
+            return res           
+        else :
+            print("rajouter une erreur pour quand on a mal configuré le parcours, sinon, ça foire")
+            a=1/0
+    
+    def traite_doigts (self, L_badgeuse, equipe):
+        """
+        fonction qui s'assure que la liste des doigts est mis sous le bon format 
+        L_badgeuse est une liste de couple (badgeuse, temps) associé à une épreuve
+        on vérifie également que toutes les badgeuses obligatoires sont bien présentes
+        equipe est le numéro de l'équipe, il sert pour le débuggage
+        """
+
+
+        L_badgeuse_final = []
+
+        D={}
+        for (badgeuse, temps) in L_badgeuse:
+            if badgeuse.signaleur in [self.ordre_badgeuse[i].signaleur for i in range(len(self.ordre_badgeuse))]:
+                if badgeuse.signaleur not in D:
+                    D[badgeuse.signaleur] = []
+                D[badgeuse.signaleur].append((badgeuse,temps))
+
+
+        #on traite ici le cas spécial des mass start
+        if self.ordre_badgeuse[0].h_mass_start not in ["", "nan"] and pd.notna(self.ordre_badgeuse[0].h_mass_start) : # and self.ordre_badgeuse[0].h_mass_start 
+            try :
+                L_badgeuse_final.append((self.ordre_badgeuse[0], u.sec_from_heure(self.ordre_badgeuse[0].h_mass_start)))
+            except:
+                raise ValueError(f"erreur dans la construction de l'épreuve {self.nom}, on a {[(L_badgeuse[i][0].numero,L_badgeuse[i][1]) for i in range(len(L_badgeuse))]} pour l'equipe {equipe}")
+
+
+        for signaleur in D.keys():
+            badgeuse_associe = D[signaleur][0][0]
+            if not badgeuse_associe.repetable:
+                if badgeuse_associe.fonction == "gel":
+                    D[signaleur] = [D[signaleur][-1][1]]
+                elif badgeuse_associe.fonction == "degel":
+                    D[signaleur] = [D[signaleur][0][1]]
+                elif badgeuse_associe.fonction == "depart":
+                    D[signaleur] = [D[signaleur][-1][1]]
+                elif badgeuse_associe.fonction == "fin":
+                    D[signaleur] = [D[signaleur][0][1]]
+                elif badgeuse_associe.fonction == "gel_1_point":
+                    D[signaleur] = [D[signaleur][-1][1]]
+                else:
+                    raise ValueError(f"la badgeuse {badgeuse_associe.numero} n'est pas répétable et n'a pas de fonction valide")
+                L_badgeuse_final.append((badgeuse_associe,D[signaleur][0]))
+            else:
+                for occurence in D[signaleur]:
+                    L_badgeuse_final.append(occurence)
+
+        self.count_badgeuse(L_badgeuse_final, equipe)
+
+        return L_badgeuse_final
+        
+class data_bo(data_epreuve_avec_doigts):
+
+    def __init__(self, nom, L_badgeuse):   
+        super().__init__(nom)
+        self.type = "bo"
+        self.check(nom, L_badgeuse)
+        self.ordre_badgeuse = L_badgeuse
+
+    def is_L_badgeuse_valide(self, L_badgeuse):
+        """
+        on vérifie qu'on a bien un départ et une arrivée, si un gel est toujours suivi d'un dégel unique
+        """
+        if len(L_badgeuse)>=1:
+            res = L_badgeuse[0].fonction == "depart" and L_badgeuse[-1].fonction == "fin" 
+            i = 1
+            while i <len(L_badgeuse) and res:
+                if L_badgeuse[i].fonction == "gel":
+                    res = L_badgeuse[i+1].fonction == "degel"
+                    i+=2
+                else:
+                    i+=1
+            return res           
+        else :
+            raise ValueError(f"erreur dans la construction de l'épreuve {self.nom}, on a {[L_badgeuse[i].numero for i in range(len(L_badgeuse))]}")
+    
+     
+    def traite_doigts(self, L_badgeuse, equipe):
+        """
+        fonction qui s'assure que la liste des doigts est mis sous le bon format 
+        L_badgeuse est une liste de couple (badgeuse, temps) associé à une épreuve
+        on vérifie également que toutes les badgeuses obligatoires sont bien présentes
+        equipe est le numéro de l'équipe, il sert pour le débuggage
+        """
+
+
+        L_badgeuse_final = []
+
+        D={}
+        for (badgeuse, temps) in L_badgeuse:
+            if badgeuse.signaleur in [self.ordre_badgeuse[i].signaleur for i in range(len(self.ordre_badgeuse))]:
+                if badgeuse.signaleur not in D:
+                    D[badgeuse.signaleur] = []
+                D[badgeuse.signaleur].append((badgeuse,temps))
+
+        #on traite ici le cas spécial des mass start
+        if self.type == "obli" and self.ordre_badgeuse[0].h_mass_start != '':
+            L_badgeuse_final.append((self.ordre_badgeuse[0], u.sec_from_heure(self.ordre_badgeuse[0].h_mass_start)))
+
+
+
+        for signaleur in D.keys():
+            badgeuse_associe = D[signaleur][0][0]
+            if badgeuse_associe.fonction == "gel":
+                D[signaleur] = [D[signaleur][-1][1]]
+            elif badgeuse_associe.fonction == "degel":
+                D[signaleur] = [D[signaleur][0][1]]
+            elif badgeuse_associe.fonction == "depart":
+                D[signaleur] = [D[signaleur][0][1]]
+            elif badgeuse_associe.fonction == "fin":
+                D[signaleur] = [D[signaleur][-1][1]]
+            elif badgeuse_associe.fonction == "gel_1_point":
+                D[signaleur] = [D[signaleur][-1][1]]
+            else:
+                raise ValueError(f"la badgeuse {badgeuse_associe.numero} n'est pas répétable et n'a pas de fonction valide")
+            L_badgeuse_final.append((badgeuse_associe,D[signaleur][0]))
+
+
+        self.count_badgeuse(L_badgeuse_final, equipe)
+
+        return L_badgeuse_final
+
+
+class data_Grimpeur(data_epreuve_avec_doigts):
+
+    def __init__(self, nom, participation,L_badgeuse):   
+        super().__init__(nom)
+        self.type = "grimpeur"
+        self.ordre_badgeuse = None
+        self.participation = participation #participation représente le nombre de points gagnés par le premier du grimpeur 
+        self.check(nom, L_badgeuse)
+        self.ordre_badgeuse = L_badgeuse
+
+
+    def is_L_badgeuse_valide(self, L_badgeuse):
+        """
+        on vérifie qu'on a bien un départ et une arrivée (on n'autorise pas les gels/dégels sur les grimpeurs)
+        """
+        if len(L_badgeuse)==2: 
+            return (L_badgeuse[0].fonction == "depart" and L_badgeuse[-1].fonction == "fin")
+        else :
+            print("rajouter une erreur pour quand on a mal configuré le parcours, sinon, ça foire")
+            a=1/0
+    
+    def traite_doigts (self, L_badgeuse, equipe):
+        if L_badgeuse[0][0].fonction == "depart" and L_badgeuse[-1][0].fonction == "fin":
+            self.count_badgeuse(L_badgeuse, equipe)
+            return L_badgeuse
+        else:
+            raise ValueError(f"erreur dans la construction de l'épreuve {self.nom}, on a {[(L_badgeuse[i][0].numero,L_badgeuse[i][1]) for i in range(len(L_badgeuse))]} pour l'equipe {equipe}")
+
+
+class data_parcours():
+    """
+    classe qui a pour but de stocker toutes les infos du parcours parès les avoir lu sur le csv. Les épreuves seront stockés via les classes au-dessus
+    tout celà est indépendant de ce qu'il y a au-dessus
+    rajouter une badgeuse 0 dans le fichier des badgeuse si on a une masse start
+    """
+    def __init__(self):
+
+        df = pd.read_csv("Data_equipes_parcours/POLIS_parcours.csv", sep=";")
+        self.epreuves = []
+        for i in range(df.shape[0]):
+            row = df.iloc[i]
+            if row["type"] == "acti":
+                epreuve = data_epreuve_acti(row["nom"], int(row["participation"]), int(row["victoire"]), int(row["record"]))
+            elif row["type"] == "co":
+                epreuve = data_epreuve_co(row["nom"], row["participation"])
+            else: 
+                POLIS_badgeuse = pd.read_csv("Data_equipes_parcours/POLIS_badgeuses.csv", sep=";")
+                L_badgeuse = self.create_L_badgeuse(row["nom"], POLIS_badgeuse)
+                if row["type"] == "Obli":
+                    epreuve = data_obli(row["nom"], L_badgeuse)
+                elif row["type"] == "bo":
+                    epreuve = data_bo(row["nom"], L_badgeuse)
+                elif row["type"] == "Grimpeur":
+                    epreuve = data_Grimpeur(row["nom"], row["participation"], L_badgeuse)
+                else:
+                    print(f"Erreur : le type {row['type']} n'est pas un type d'épreuve valide")
+                    a=1/0
+            # if epreuve.nom == "Obli 2":
+            #     print(epreuve.nom, epreuve.ordre_badgeuse[0].h_mass_start)
+            #     a=1/0
+            self.epreuves.append(epreuve)
+
+    def create_L_badgeuse(self, nom, POLIS_badgeuse):
+        """
+        on crée la liste des badgeuses pour une épreuve donnée
+        """
+        L_badgeuse = []
+        for i in range(POLIS_badgeuse.shape[0]):
+            row = POLIS_badgeuse.iloc[i]
+            if row["epreuve"] == nom:
+                L_badgeuse.append(Badgeuse(row["numero"], row["fonction"], row["signaleur"], gain_temps=row["temps bonus"], obligatoire= pd.notna(row["obligatoire"]), repetable= int(row["repetable"]), h_mass_start=row["heure mass start"]))
+
+        return L_badgeuse
+
+    def get_badgeuse(self, res_clear):
+
+        """
+        fonction qui, à chaque occurence d'une badgeuse, associe la bonne badgeuse. 
+        """
+
+        res = []           
+        D_occurence = {num_badgeuse:0 for (num_badgeuse, temps) in res_clear}
+        for num_badgeuse, temps in res_clear:
+            D_occurence[num_badgeuse] += 1
+            for epreuve in self.epreuves:
+                if epreuve.type in ["obli", "bo", "grimpeur"]:
+                    for badgeuse in epreuve.ordre_badgeuse:
+                        if epreuve.type == "bo":
+                            L= epreuve.ordre_badgeuse
+                        if badgeuse.numero == num_badgeuse:
+                            if badgeuse.repetable == D_occurence[badgeuse.numero]:
+                                res.append((badgeuse, temps))
+
+        return res 
